c5fcb1b10f3c95faa8cc0101cfd493171e71636f
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-04-10 02:25:59 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -14,3 +14,25 @@ def merge(L1, L2):
         temp = temp.next 
     temp.next = L2
     return L1
+
+
+'''
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+'''
+
+def deleteAtTail(head):
+    if head == None or head.next == None:
+        return None 
+    prev = None 
+    cur = head 
+
+    # move the cur node till last node
+    while cur.next != None:
+        prev = cur 
+        cur = cur.next
+    # make second last node next to none
+    prev.next = None
+    return head
