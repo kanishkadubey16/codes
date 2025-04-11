a1d62e861d0a06c09b6f7bd19bb171f7e902ea87
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-04-12 01:02:06 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -91,4 +91,40 @@ def insertnew(head, val , pos):
     return head
 
 
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+
+    def print_list(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+my_list = LinkedList()
+my_list.insert_at_end(10)
+my_list.insert_at_end(20)
+my_list.insert_at_end(30)
+my_list.print_list()
+
+
+
 # thinkl for the edge case
