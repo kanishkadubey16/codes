235422d98c6444bc64e778b3dc770ac47596c590
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-04-20 01:02:40 +0530

    Update random.py

--- random.py
+++ random.py
@@ -119,4 +119,10 @@ class Solution:
                     l[i] = 0
                     i+=1
         return len(s) - l[-1]
-                
+
+
+def areRotations(self,s1,s2):
+    if len(s1) != len(s2):
+        return False
+    return s2 in (s1 + s1)
+            
