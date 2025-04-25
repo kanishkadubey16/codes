659105dc71b252c543f31b649be0d702b7d45589
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-04-26 04:12:54 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -189,5 +189,24 @@ def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         return head
 
 
+class Solution:
+    def detectCycle(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:  
+                break
+        else:
+            return None 
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        
+        return slow
+
 
 # thinkl for the edge case
