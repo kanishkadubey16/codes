fa381e01395cb5dd3609f84f52858a35eaebe5c4
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-04-26 04:10:19 +0530

    Update random.py

--- random.py
+++ random.py
@@ -190,4 +190,27 @@ class Solution:
         if not inserted:
             result.append(newInterval)
         return result
-            
+
+
+class Solution:
+    def insertInterval(self, intervals, newInterval):
+        result = []
+        inserted = False
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            elif interval[0] > newInterval[1]:
+                if not inserted:
+                    result.append(newInterval)
+                    inserted = True 
+                result.append(interval)
+            else:
+                newInterval[0] = min(newInterval[0],interval[0])
+                newInterval[1] = max(newInterval[1],interval[1])
+        if not inserted:
+            result.append(newInterval)
+        return result
+
+
+
+
