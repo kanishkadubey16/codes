c31765327e660676f57ebb315cb0466f9f47b27e
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-02 04:34:36 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -209,4 +209,14 @@ class Solution:
         return slow
 
 
+
+def check(node,val):
+        if node is None:
+            return True 
+        if node.val != val:
+            return False 
+        return check(node.left,val) and check(node.right,val)
+    return check(root,root.val)
+
+
 # thinkl for the edge case
