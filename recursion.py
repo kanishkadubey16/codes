4d8bcfbb79d479a61ef6e226f80dc24f7541a0b3
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-07 01:00:59 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -58,3 +58,17 @@ class Solution:
 n = int(input().strip())  
 nums = list(map(int, input().split()))
 can_jump(nums)
+
+
+n, k = map(int, input().split())
+treasure = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = treasure[0]
+
+for i in range(1, n):
+    option1 = dp[i - 1]
+    option2 = treasure[i]
+    if i - k - 1 >= 0:
+        option2 += dp[i - k - 1]
+    dp[i] = max(option1, option2)
+print(dp[n - 1])
