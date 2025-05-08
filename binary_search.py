6ec792f9b9f96f29edbc843e1495f6de3b722bff
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-09 02:00:03 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -40,3 +40,11 @@ class Solution:
             else:
                 h = mid - 1
         return -1
+
+
+if root is None:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
