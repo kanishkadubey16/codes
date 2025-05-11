ad140805384a35c1b2b6f8fcddd50036fc563c30
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-12 01:31:57 +0530

    Update random.py

--- random.py
+++ random.py
@@ -352,6 +352,17 @@ stack = []
 
 
 
+class Solution:
+	def twoSum(self, arr, target):
+	    seen = set()
+	    for i in arr:
+	        if target - i in seen:
+	            return True 
+            seen.add(i)
+        return False
+
+
+
 
 
 
