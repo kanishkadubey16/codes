eaf60398c6eea92bf63174d703dc104fc7f15c6d
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-15 22:54:18 +0530

    Update random.py

--- random.py
+++ random.py
@@ -390,6 +390,48 @@ class Solution:
 
         return triplets
 
+n, m = map(int, input().split())
+mat = []
+for _ in range(n):
+    mat.append(list(map(int, input().split())))
+
+
+top = 0
+bottom = n - 1
+left = 0
+right = m - 1
+
+
+result = []
+
+
+while top <= bottom and left <= right:
+
+
+    for i in range(left, right + 1):
+        result.append(mat[top][i])
+    top += 1
+
+
+    for i in range(top, bottom + 1):
+        result.append(mat[i][right])
+    right -= 1
+
+
+    if top <= bottom:
+        for i in range(right, left - 1, -1):
+            result.append(mat[bottom][i])
+        bottom -= 1
+
+
+    if left <= right:
+        for i in range(bottom, top - 1, -1):
+            result.append(mat[i][left])
+        left += 1
+
+
+print(*result)
+
 
 
 
