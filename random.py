28f46664febc2bb92024b13d2daa9ba0097d815c
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-18 23:40:25 +0530

    Update random.py

--- random.py
+++ random.py
@@ -457,6 +457,15 @@ def longestConsecutive(self,arr):
                 longest = max(longest,c)
         return longest
 
+class Solution:
+
+    def anagrams(self, arr):
+        from collections import defaultdict
+        anagram_map = defaultdict(list)
+        for word in arr:
+            key = "".join(sorted(word))
+            anagram_map[key].append(word)
+        return list(anagram_map.values())
 
 
 
