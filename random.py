550032d6b0e5c5590891522de6fde1539cd1df76
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-05-25 22:25:27 +0530

    Update random.py

--- random.py
+++ random.py
@@ -490,6 +490,22 @@ reversed_number = int(reversed_n)
 print(reversed_number)
 
 
+def countTriangles(self, arr):
+        n = len(arr)
+        arr.sort()
+        c = 0
+        for k in range(n-1,1,-1):
+            i = 0
+            j = k - 1 
+            while i < j:
+                if arr[i] + arr[j] > arr[k]:
+                    c += (j - i)
+                    j -= 1 
+                else:
+                    i += 1
+        return c
+
+
 
 
 
