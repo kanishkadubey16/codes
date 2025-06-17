c432c53f58e6319f3826e114a83179a18d2237c4
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-06-17 23:35:07 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,20 +1,4 @@
-'''
-class Node:
-    def __init__(self, x):
-        self.data = x
-        self.next = None
-'''
-def merge(L1, L2):
-    if L1 == None:
-        return L2
-    if L2 == None:
-        return L1 
-    temp = L1
-    while temp.next != None:
-        temp = temp.next 
-    temp.next = L2
-    return L1
-class Solution:
+
     #Function to remove a loop in the linked list.
     def removeLoop(self, head):
         slow = fast = head 
