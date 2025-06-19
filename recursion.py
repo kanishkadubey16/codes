5c888a9ab33d3fc9b65336bc2b4286738d14f550
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-06-19 22:57:19 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -25,3 +25,19 @@ for i in range(1, n):
         option2 += dp[i - k - 1]
     dp[i] = max(option1, option2)
 print(dp[n - 1])
+
+
+
+
+n, k = map(int, input().split())
+treasure = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = treasure[0]
+
+for i in range(1, n):
+    option1 = dp[i - 1]
+    option2 = treasure[i]
+    if i - k - 1 >= 0:
+        option2 += dp[i - k - 1]
+    dp[i] = max(option1, option2)
+print(dp[n - 1])
