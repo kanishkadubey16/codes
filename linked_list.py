e2bfd0f825787823b40da0d1dff06f9200a91a37
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-06-20 23:36:15 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -88,24 +88,7 @@ def make_circular(head):
 
 
 
-class Solution:
-    def sortedMerge(self,head1, head2):
-        ans = Node(0)
-        tail = ans 
-        while head1 and head2:
-            if head1.data < head2.data:
-                tail.next = head1 
-                head1 = head1.next 
-            else:
-                tail.next = head2 
-                head2 = head2.next 
-            tail = tail.next 
-        
-        if head1:
-            tail.next = head1 
-        if head2:
-            tail.next = head2 
-        return ans.next
+
 
 
 
