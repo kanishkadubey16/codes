4f806791676fbfc8144e6c47ee358aa7bca0fe7e
Kanishka Dubey <kanishkadubey16@gmail.com>
2025-07-08 23:23:40 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -25,12 +25,7 @@ print(max(countN,countP))
 
 
 
-if root is None:
-            return None
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-        return root
+
 
 
 
